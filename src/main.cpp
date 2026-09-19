b73c354ede03a4714804a07ac98f44b46a8981a1
chmodmasx <daninicob@gmail.com>
2026-09-19 10:16:21 -0300

    feat: run PSD shell as Wayland layer surface

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,21 @@
 #include "core/SpatialMotionController.h"
 #include "core/SpatialState.h"
 
+#include <LayerShellQt/shell.h>
+#include <LayerShellQt/window.h>
+
 #include <QCoreApplication>
 #include <QGuiApplication>
 #include <QLoggingCategory>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QQuickWindow>
 #include <QUrl>
 
 int main(int argc, char *argv[])
 {
+    LayerShellQt::Shell::useLayerShell();
+
     QGuiApplication application(argc, argv);
     QCoreApplication::setApplicationName(QStringLiteral("psd-shell"));
     QCoreApplication::setApplicationVersion(QStringLiteral("0.1.0"));
@@ -46,7 +52,31 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    auto *window = qobject_cast<QQuickWindow *>(engine.rootObjects().constFirst());
+    if (!window) {
+        qCritical() << "PSD QML root is not a QQuickWindow";
+        return EXIT_FAILURE;
+    }
+
+    auto *layerWindow = LayerShellQt::Window::get(window);
+    if (!layerWindow) {
+        qCritical() << "PSD failed to create its layer-shell surface";
+        return EXIT_FAILURE;
+    }
+
+    layerWindow->setScope(QStringLiteral("psd-shell"));
+    layerWindow->setLayer(LayerShellQt::Window::LayerBackground);
+    layerWindow->setAnchors(
+        LayerShellQt::Window::AnchorTop
+        | LayerShellQt::Window::AnchorBottom
+        | LayerShellQt::Window::AnchorLeft
+        | LayerShellQt::Window::AnchorRight);
+    layerWindow->setExclusiveZone(-1);
+    layerWindow->setKeyboardInteractivity(
+        LayerShellQt::Window::KeyboardInteractivityOnDemand);
+
     compositorBridge.start();
+    window->show();
 
     return application.exec();
 }
