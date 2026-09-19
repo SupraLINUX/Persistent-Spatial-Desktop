b3e165a62fa778a242748a7d21c6f0d2dff1370e
chmodmasx <daninicob@gmail.com>
2026-09-19 06:34:23 -0300

    feat: add Hyprland IPC compositor bridge

---
+++ src/compositor/HyprlandIpcBridge.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "compositor/CompositorBridge.h"
+
+#include <QByteArray>
+#include <QLocalSocket>
+#include <QTimer>
+
+#include <functional>
+
+class HyprlandIpcBridge final : public CompositorBridge
+{
+    Q_OBJECT
+    Q_PROPERTY(QString instanceSignature READ instanceSignature NOTIFY instanceSignatureChanged)
+
+public:
+    explicit HyprlandIpcBridge(QObject *parent = nullptr);
+
+    [[nodiscard]] QString backendName() const override;
+    [[nodiscard]] QString instanceSignature() const;
+
+    void start() override;
+    Q_INVOKABLE void refreshAll() override;
+
+signals:
+    void instanceSignatureChanged();
+
+private:
+    enum RefreshFlag : quint8 {
+        RefreshNone = 0,
+        RefreshMonitors = 1 << 0,
+        RefreshWorkspaces = 1 << 1,
+        RefreshWindows = 1 << 2,
+        RefreshEverything = RefreshMonitors | RefreshWorkspaces | RefreshWindows,
+    };
+
+    using JsonCallback = std::function<void(const QByteArray &)>;
+
+    void discoverInstance();
+    void connectEventStream();
+    void handleEventData();
+    void handleEventLine(const QByteArray &line);
+    void scheduleRefresh(quint8 flags);
+    void refreshPending();
+    void refreshMonitors();
+    void refreshWorkspaces();
+    void refreshWindows();
+    void requestJson(const QByteArray &request, JsonCallback callback);
+
+    QString m_instanceSignature;
+    QString m_commandSocketPath;
+    QString m_eventSocketPath;
+    QLocalSocket m_eventSocket;
+    QByteArray m_eventBuffer;
+    QTimer m_refreshTimer;
+    QTimer m_reconnectTimer;
+    quint8 m_pendingRefresh = RefreshNone;
+};
